092d4b6c62d5f6279be8f7a16d6002b9af770c1b
Adarsh-Singh-Bhadauria <adarshbhadauria5936@gmail.com>
2023-11-03 11:54:57 +0530

    footer added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import About from './components/About';
+import Footer from './components/Footer';
 import Home from './components/Home';
 import Navbar from './components/Navbar';
 import {Routes, Route} from 'react-router-dom';
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/about" element={<About/>} />
       </Routes>
+      <Footer/>
     </>
   );
 }

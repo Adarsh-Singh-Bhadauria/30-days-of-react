cafb076367a9c80115d1b5fe71a5e2e3fba02185
Adarsh-Singh-Bhadauria <adarshbhadauria5936@gmail.com>
2023-11-06 08:44:34 +0530

    added product component along with passing props in it

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 const Navbar = () => {
   return (
     <>
-    <div className='flex justify-between space-x-8 m-4 bg-slate-100 rounded-md py-1 m-4'>
+    <div className='flex justify-between space-x-8 bg-slate-100 rounded-md py-1 m-4'>
       <div className='text-2xl font-extrabold px-2'>REACT</div>
         <ul className='flex space-x-8 text-lg font-semibold'>
           <Link to="/" className='hover:bg-red-200 px-2 rounded-sm'>HOME</Link>
---
+++ src/components/Product.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Product = (props) => {
+  return (
+    <>
+        <div className='flex flex-col text-center space-y-2 items-center justify-center p-2 bg-green-100 rounded-md'>
+            <img className='rounded-md w-48' src={props.image} alt='product' />
+            <p className='font-bold'>{props.title}</p>
+            <p>{props.description}</p>
+        </div>
+    </>
+  )
+}
+
+export default Product

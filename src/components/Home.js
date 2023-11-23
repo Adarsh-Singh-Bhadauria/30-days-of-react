07121426487cb6ed033a1e2bbdce7f3e4b07faec
Adarsh-Singh-Bhadauria <adarshbhadauria5936@gmail.com>
2023-11-23 11:11:12 +0530

    removed extra fragments

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
     <>
       <Header/>
       <div className='m-4 mt-8 p-2 bg-blue-100 w-fit rounded-md'>
-          Hello, This project will show my development skills in React throughout this 30 day journey.
+        Hello, This project will show my development skills in React throughout this 30 day journey.
       </div>
     </>
   )

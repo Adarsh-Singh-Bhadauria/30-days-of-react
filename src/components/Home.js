2c7e6363464738e7bbaea3982b4b60d9173952ad
Adarsh-Singh-Bhadauria <adarshbhadauria5936@gmail.com>
2023-11-23 11:13:58 +0530

    removed extra fragments

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,12 @@ import Header from './Header'
 
 const Home = () => {
   return (
-    <>
     <div className='h-[100vh]'>
       <Header/>
       <div className='m-4 mt-8 p-2 bg-blue-100 w-fit rounded-md'>
         Hello, This project will show my development skills in React throughout this 30 day journey.
       </div>
     </div>
-    </>
   )
 }
 
